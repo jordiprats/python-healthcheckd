eea68dbbae3249ae80e5cdba3528138d58e52d53
jordiprats <jordi.prats@gmail.com>
2019-03-10 12:35:53 +0100

    detalls

--- healthcheckd.py
+++ healthcheckd.py
@@ -1,5 +1,6 @@
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import subprocess
+from ConfigParser import SafeConfigParser
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
 PORT_NUMBER = 17
 
@@ -10,14 +11,9 @@ class HealthCheckHandler(BaseHTTPRequestHandler):
     def check_supervisord_status(self):
         # supervisorctl status | awk '{ print $2 }' | sort | uniq | wc -l
         p = subprocess.Popen("/bin/true", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        linecount=0
-        lastline=""
-        for line in p.stdout.readlines():
-            lastline = line.strip()
-            linecount+=1
         retval = p.wait()
 
-        return (retval==0 and linecount==1 and lastline=="OK")
+        return retval==0
 
 
     def do_healthcheck(self):
@@ -44,15 +40,24 @@ class HealthCheckHandler(BaseHTTPRequestHandler):
         self.do_healthcheck()
         return
 
-try:
-    #Create a web server and define the handler to manage the
-    #incoming request
-    server = HTTPServer(('', PORT_NUMBER), HealthCheckHandler)
-    print 'Started httpserver on port ' , PORT_NUMBER
+if __name__ == "__main__":
+    try:
+        configfile = sys.argv[1]
+    except IndexError:
+        configfile = './healthcheckd.config'
 
-    #Wait forever for incoming htto requests
-    server.serve_forever()
+    with PidFile('healthcheckd') as pidfile:
+        logging.basicConfig(level=logging.DEBUG,
+                            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        try:
+            #Create a web server and define the handler to manage the
+            #incoming request
+            server = HTTPServer(('', PORT_NUMBER), HealthCheckHandler)
+            print 'Started httpserver on port ' , PORT_NUMBER
 
-except KeyboardInterrupt:
-    print 'shutting down the web server'
-    server.socket.close()
+            #Wait forever for incoming htto requests
+            server.serve_forever()
+
+        except KeyboardInterrupt:
+            print 'shutting down healthcheckd'
+            server.socket.close()
